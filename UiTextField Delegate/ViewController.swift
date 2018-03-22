c0e9ffe4ea0df9dbbac719f03723e4eec29cf28b
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-03-22 11:47:46 +0900

    UITextField

--- UiTextField Delegate/ViewController.swift
+++ UiTextField Delegate/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hello: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.placeholder = "입력하세요." //텍스트필드뷰에 글씨를 나타낸다.
+        textField.clearButtonMode = UITextFieldViewMode.whileEditing // 입력창에 x모양 추가
     }
-
+    
+    @IBAction func button(_ sender: Any) {
+        hello.text = "Hello " + textField.text! //Hello label에 글자적은뒤 버튼을 누르면 추가됨
+        textField.text = "" //버튼을 한번더 누를시 초기화
+        textField.resignFirstResponder() //버튼을 누르면 키패드가 내려간다.
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
