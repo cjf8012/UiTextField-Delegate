691d144d177c5825c3f909b437b20841881fa812
박재호 | ParkJaeHo <37130344+cjf8012@users.noreply.github.com>
2018-03-28 12:56:11 +0900

    Update ViewController.swift

--- UiTextField Delegate/ViewController.swift
+++ UiTextField Delegate/ViewController.swift
@@ -1,37 +1,49 @@
 //
 //  ViewController.swift
-//  UiTextField Delegate
+//  UITextField Deligate01
 //
-//  Created by D7703_16 on 2018. 3. 22..
-//  Copyright © 2018년 D7703_16. All rights reserved.
+//  Created by D7702_10 on 2018. 3. 22..
+//  Copyright © 2018년 ksh. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var hello: UILabel!
-    @IBOutlet weak var textField: UITextField!
+class ViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var lab: UILabel!
+    @IBOutlet weak var labs: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        textField.placeholder = "입력하세요." //텍스트필드뷰에 글씨를 나타낸다.
-        textField.clearButtonMode = UITextFieldViewMode.whileEditing // 입력창에 x모양 추가
+        labs.delegate = self
+        labs.placeholder = "입력을 하세요!"
+        labs.clearButtonMode = UITextFieldViewMode.whileEditing
+        
     }
-    
-    @IBAction func button(_ sender: Any) {
-        hello.text = "Hello " + textField.text! //Hello label에 글자적은뒤 버튼을 누르면 추가됨
-        textField.text = "" //버튼을 한번더 누를시 초기화
-        textField.resignFirstResponder() //버튼을 누르면 키패드가 내려간다.
+    @IBAction func bt(_ sender: Any) {
+        lab.text = "Hello, " + labs.text!
+        labs.text = ""
+        labs.resignFirstResponder()
+        //view를 터치하면 키패드가 내려감
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        labs.resignFirstResponder()
+        view.backgroundColor = UIColor.black
+    }
+        //UITextFieldDelegate Method
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        labs.resignFirstResponder()
+        view.backgroundColor = UIColor.red
+        return true
+        
+    }
+        // clear button을 눌렀을때 호출
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        labs.backgroundColor = UIColor.darkGray
+        return true
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
 }
-
